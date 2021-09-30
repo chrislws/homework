3bca53b60e2fec0aeb796c79e9b7d53dd0547769
chrislws <90813654+chrislws@users.noreply.github.com>
2021-09-30 15:09:04 +0800

    Add files via upload

---
+++ GO/httpserver2.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os" //我们要用到os包中的env
+)
+func main() {
+	var VERSION string
+	os.Setenv("VERSION","v1.0.0")
+	VERSION = os.Getenv("VERSION")
+	fmt.Println(VERSION)
+}
+
+func VERSION(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("v1.0.0"))
+	http.HandleFunc("/VERSION", VERSION)
+}
